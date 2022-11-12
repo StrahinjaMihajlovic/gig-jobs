1d706765a2f1bc15d26c1cdf22c666fc9dd3b661
Strahinja <strale@hotmail.com>
2022-11-12 23:04:59 +0100

    Filled models with adequate relationships and configs

--- app/Models/Gig.php
+++ app/Models/Gig.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Gig extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'company_id'
+    ];
+
+    protected $casts = [
+        'timestamp_start' => 'datetime',
+        'timestamp_end' => 'datetime'
+    ];
+
+    /**
+     * Returns a company this gig belongs to
+     * @return BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
