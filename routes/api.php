1f47b623bc4c1b567c17e29ee9533f125acb8c62
Strahinja <strale@hotmail.com>
2022-11-12 18:47:28 +0100

    Implemented user login procedure

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::put('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
