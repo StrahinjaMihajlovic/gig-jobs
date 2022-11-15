3dcdb4bc0da3d6a92c9cdbc484d6b0a7f5ffd3e7
Strahinja <strale@hotmail.com>
2022-11-15 19:25:40 +0100

    Implemented scope to model for gigs search

--- app/Models/Gig.php
+++ app/Models/Gig.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Company;
+use App\Models\Company;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Gig extends Model
 {
@@ -30,5 +32,37 @@ class Gig extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function scopeByCompanyString(Builder $query, $companyString) 
+    {
+        return $query->whereRelation('company', 'name', 'like', "%$companyString%");
+    }
+
+    public function scopeByStatus(Builder $query, $status) 
+    {
+        return $query->where('status', $status);
+    }
+
+    public function scopeByProgress(Builder $query, $progress)
+    {
+        $query->where(function (Builder $query) use ($progress) {
+            $time = Carbon::now();
+            
+            if ($progress === 'not_started') {
+                $query->orWhere('timestamp_start', '>=', $time);
+            }
+
+            if ($progress === 'started') {
+                $query->orWhere(function(Builder $query) use ($time) {
+                    $query->where('timestamp_start', '<', $time)
+                    ->where('timestamp_end', '>', $time);
+                });
+            }
+
+            if ($progress === 'finished') {
+                $query->orWhere('timestamp_end', '<=', $time);     
+            }
+        });
     }
 }
