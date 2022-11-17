aaf866e54df2755d59e908037b0f82fe83c8559a
Strahinja <strale@hotmail.com>
2022-11-17 21:44:47 +0100

    Made factories for Gigs and Companies

--- database/factories/GigFactory.php
+++ database/factories/GigFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class GigFactory extends Factory
 {
@@ -14,7 +15,13 @@ class GigFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => Str::random(100),
+            'description' => Str::random(500),
+            'timestamp_start' => $this->faker->dateTimeBetween('this_week', '+5 days'),
+            'timestamp_end' => $this->faker->dateTimeBetween('+5 days', '+15 days'),
+            'number_of_positions' => $this->faker->randomNumber(2),
+            'pay_per_hour' => $this->faker->randomFloat(2, 0, 20),
+            'status' => $this->faker->numberBetween(0, 1),
         ];
     }
 }
