c84583369f2c112bf2e8f36ea7e42122f9d1a229
Strahinja <strale@hotmail.com>
2022-11-17 21:46:04 +0100

    Made seeder for populating the DB with appropriate data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Gig;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use \Faker\Generator;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+        ->count(10000)
+        ->create()
+        ->each(function (User $user) {
+            $user->companies()
+            ->saveMany(Company::factory()
+            ->count((new Generator())->numberBetween(1,3))
+            ->create(['user_id' => $user->id])
+            ->each( function (Company $company) {
+                $company->gigs()
+                ->saveMany(Gig::factory()
+                ->count((new Generator())->numberBetween(2,5))
+                ->create(['company_id' => $company->id]));
+            }));
+        });
     }
 }
